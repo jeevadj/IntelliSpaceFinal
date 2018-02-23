2ffe0c44d2a87136feb59f75cf9ac78e1073bf8e
jeeva2702 <d.jeeva6@gmail.com>
2018-02-23 10:31:25 +0530

    Latest Commit

--- app/src/main/java/com/example/sasi/intellispace/rcActivity.java
+++ app/src/main/java/com/example/sasi/intellispace/rcActivity.java
@@ -291,7 +291,7 @@ public class rcActivity extends AppCompatActivity
                                         String[] splited3 = BookingAdapter.ET.split(":");
                                         int  EndTime= Integer.parseInt(splited3[0]);
                                         int  StartTime= Integer.parseInt(splited2[0]);
-                                        if(cloudStartTime==StartTime){
+                                        if(cloudStartTime==StartTime||cloudEndTime>StartTime){
                                             checkfloor=floor;
                                             checkroom=room.getKey();
 
